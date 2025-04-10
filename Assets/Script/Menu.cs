2d71f34d1ae77c7d89a11a9fa1d4925677827e83
TimotiusEkanaT <117902158+TimotiusEkanaT@users.noreply.github.com>
2025-04-10 10:03:35 +0700

    Commit_Assets

---
+++ Assets/Script/Menu.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public bool isEscapeToExit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.Escape)){
+            if(isEscapeToExit){
+                Application.Quit();
+            } else {
+                KembaliKeMenu();
+            }
+        }
+    }
+        public void MulaiPermainan()
+    {
+        SceneManager.LoadScene("Gameplay");
+    }
+    public void KembaliKeMenu()
+    {
+        SceneManager.LoadScene("GameMenu");
+    }
+}
